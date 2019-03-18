02bc6dba83d9ae84ef53d80575edab3be65f88a8
CarineIzere <carizeree@gmail.com>
2019-03-18 13:31:15 +0200

    add ROOT_URLCONF in settings.py

--- MyPicture/settings.py
+++ MyPicture/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap3'
+    # 'bootstrap3'
     'picture.apps.PictureConfig',
     'django.contrib.admin',
     'django.contrib.auth',
