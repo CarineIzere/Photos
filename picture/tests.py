a525804db606263964f618b9949fe7f3568a80bc
CarineIzere <carizeree@gmail.com>
2019-03-22 12:28:01 +0200

    location

--- picture/tests.py
+++ picture/tests.py
@@ -3,11 +3,11 @@ from .models import Image,Location,Category
 # Create your tests here.
 class MyGalery_TestCases(TestCase):
     def setUp(self):
-        self.new_category = Category(cat_name='Dance')
+        self.new_category = Category(cat_name='')
         self.new_category.save_category()
-        self.new_location = Location(location_name = 'Mombasa')
+        self.new_location = Location(location_name = '')
         self.new_location.save_location()
-        self.new_image = Image(id=1,image_name='learn', image_description='Learn 2017',image_path='media/gallery/dance-3134828_1920.jpg',image_category=self.new_category,image_location=self.new_location)
+        self.new_image = Image(id=1,image_name='learn', image_description='Learn 2019',image_path='media/gallery/dance-3134828_1920.jpg',image_category=self.new_category,image_location=self.new_location)
     
     def tearDown(self):
         Category.objects.all().delete()
